4fff999d8f10d87dc3ce0784ff5340e05e761a1d
Callie Stump <callie@stu.mp>
2021-01-15 15:13:47 -0500

    adds AddFlavor functionality to 'Treats' Views and Controllers

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -3,6 +3,7 @@ using Bakery.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
@@ -72,6 +73,23 @@ namespace Bakery.Controllers
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    [Authorize]
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0 )
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
