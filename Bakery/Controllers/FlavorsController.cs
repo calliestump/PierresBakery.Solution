a12fe2ed009ad273a651b4fa9b22cfe11a325042
Callie Stump <callie@stu.mp>
2021-01-15 16:03:33 -0500

    updates 'Flavors Views' and styling

--- Bakery/Controllers/FlavorsController.cs
+++ Bakery/Controllers/FlavorsController.cs
@@ -6,8 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
 
 namespace Bakery.Controllers
 {
--- Bakery/ViewModels/RegisterViewModel.cs
+++ Bakery/ViewModels/RegisterViewModel.cs
@@ -14,7 +14,6 @@ namespace Bakery.ViewModels
     [Display(Name = "Password")]
     public string Password { get; set; }
 
-    //[Required]
     [DataType(DataType.Password)]
     [Display(Name = "Confirm Password")]
     [Compare("Password", ErrorMessage = "The passwords don't match :(")]
